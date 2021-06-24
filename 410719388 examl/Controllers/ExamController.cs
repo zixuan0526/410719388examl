cc47f949468dbc07ce2e1127aecf9fb217b21b5f
鄭竣文 <kevin39961462@gmail.com>
2021-06-24 19:58:35 +0800

    final test

---
+++ 410719388 examl/Controllers/ExamController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _410719388_examl.Controllers
+{
+    public class ExamController : Controller
+    {
+        // GET: final
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
